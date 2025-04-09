02214ddc3bbdec3ca78dc861508c0ee2b3647dd4
levkaflower <levkaflower@gmail.com>
2025-04-09 18:35:36 +0300

    Подключение Next.js

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken')
+
+exports.verifyToken = (req, res, next) => {
+	const authHeader = req.headers.authorization
+	if (!authHeader || !authHeader.startsWith('Bearer ')) {
+		return res
+			.status(401)
+			.json({ message: 'Нет токена, авторизация запрещена' })
+	}
+	const token = authHeader.split(' ')[1]
+
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET)
+		req.user = decoded
+		next()
+	} catch (error) {
+		res.status(401).json({ message: 'Неверный токен' })
+	}
+}
+
+exports.isAdmin = (req, res, next) => {
+	if (req.user.role !== 'ADMIN') {
+		return res
+			.status(403)
+			.json({
+				message: 'Доступ запрещен. Только админ может выполнять это действие',
+			})
+	}
+	next()
+}
+exports.isUser = (req, res, next) => {
+    if (req.user.role !== 'USER') {
+        return res
+            .status(403)
+            .json({
+                message: 'Доступ запрещен. Только пользователь может выполнять это действие',
+            })
+    }
+    next()
+}
